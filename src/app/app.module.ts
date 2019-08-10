2fadfd304303907997662074fe44903551c69ac6
Leonardo Oliveira <leonardo.oliveira@basis.com.br>
2019-08-10 13:18:17 -0300

    blalala

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppTopbarComponent } from './app.topbar.component';
+import { AppMenuComponent } from './app.menu.component';
+import {MenubarModule} from 'primeng/menubar';
+import { PRIMENG_IMPORTS } from './primeng-imports';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTopbarComponent,
+    AppMenuComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    MenubarModule,
+    PRIMENG_IMPORTS
   ],
   providers: [],
   bootstrap: [AppComponent]
